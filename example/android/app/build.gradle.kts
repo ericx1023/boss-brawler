fad9d2d01c5af91f18c72be6d9b3521221848897
Sheng <ericx1023@gmail.com>
2025-05-23 12:18:57 -0700

    add icon

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_ai_toolkit_example"
+    namespace = "org.psychevalley.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_ai_toolkit_example"
+        applicationId = "org.psychevalley.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23  // Firebase requires Android 23
